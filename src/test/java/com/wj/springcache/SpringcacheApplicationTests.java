4d108ad051414af52e496c1cf157ba22ede15bc4
wwyyzz <sdn_app@163.com>
2019-04-29 07:25:45 -0700

    redis

--- src/test/java/com/wj/springcache/SpringcacheApplicationTests.java
+++ src/test/java/com/wj/springcache/SpringcacheApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,13 +17,35 @@ public class SpringcacheApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     public void contextLoads() {
         Employee employee = employeeMapper.getEmpById(1);
 
         System.out.println(employee);
 
-
     }
 
+    @Test
+    public void test01(){
+//        stringRedisTemplate.opsForValue().append("msg","hello");
+//        String msg = stringRedisTemplate.opsForValue().get("msg");
+//
+//        System.out.println(msg);
+
+        stringRedisTemplate.opsForList().leftPush("mylist","1");
+        stringRedisTemplate.opsForList().leftPush("mylist","2");
+    }
+
+    @Test
+    public void test02(){
+        Employee empById = new Employee();
+
+        redisTemplate.opsForValue().set("emp-01", empById);
+    }
 }
